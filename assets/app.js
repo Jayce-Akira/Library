a9f2e3fcef3ca91d0e17b9d8c11998564a6c2c33
Jayce <jcmestas@gmail.com>
2022-07-23 16:31:42 +0200

    correction

--- assets/app.js
+++ assets/app.js
@@ -11,20 +11,5 @@ import './styles/app.scss';
 // Doc de bootstrap et symfony
 import { Tooltip, Toast, Popover } from 'bootstrap';
 
-// app.js
-
-const $ = require('jquery');
-// this "modifies" the jquery module: adding behavior to it
-// the bootstrap module doesn't export/return anything
-require('bootstrap');
-
-// or you can include specific pieces
-// require('bootstrap/js/dist/tooltip');
-// require('bootstrap/js/dist/popover');
-
-$(document).ready(function() {
-    $('[data-toggle="popover"]').popover();
-});
-
 // start the Stimulus application
 import './bootstrap';
